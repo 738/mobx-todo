a5732ae7c8104b426d3c7fcb234f4a65606cbd91
junwooji <junwoo@finda.co.kr>
2018-09-24 14:46:53 +0900

    mobx-todo 예제 완성

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -3,8 +3,40 @@ import { observer } from 'mobx-react';
 
 @observer
 class TodoList extends React.Component {
+    createNew(e) {
+        if (e.which === 13) {
+            this.props.store.createTodo(e.target.value);
+            console.log(e);
+            e.target.value = "";
+
+        }
+    }
+
+    filter(e) {
+        this.props.store.filter = e.target.value;
+    }
+
+    toggleCompleted(todo) {
+        todo.completed = !todo.completed
+    }
+
     render() {
-        return <h1>{this.props.store.todos[0]}</h1>
+        const { clearComplete, todos, filter, filteredTodos } = this.props.store;
+        const todolist = filteredTodos.map(todo =>
+            <li key={todo.id}>
+                <input type='checkbox' onChange={this.toggleCompleted.bind(this, todo)} value={todo.completed} />
+                {todo.value}
+            </li>
+        );
+        return (
+            <div>
+                <input type='text' onKeyPress={this.createNew.bind(this)} />
+                <input type='text' value={filter} onChange={this.filter.bind(this)} />
+                <h1>TODO</h1>
+                <ul>{todolist}</ul>
+                <a href='#' onClick={clearComplete}>clear complete</a>
+            </div>
+        )
     }
 }
 
--- src/store/TodoStore.js
+++ src/store/TodoStore.js
@@ -1,8 +1,34 @@
-import { observable, autorun } from 'mobx';
+import { observable, autorun, computed } from 'mobx';
+
+class Todo {
+    @observable value;
+    @observable id;
+    @observable completed;
+
+    constructor(value) {
+        this.value = value;
+        this.id = Date.now();
+        this.completed = false;
+    }
+}
 
 class TodoStore {
-    @observable todos = ["buy milk", "buy eggs"];
+    @observable todos = [];
     @observable filter = "";
+
+    @computed get filteredTodos() {
+        let matchesFilter = new RegExp(this.filter, 'i');
+        return this.todos.filter(todo => !this.filter || matchesFilter.test(todo));
+    }
+
+    createTodo(value) {
+        this.todos.push(new Todo(value));
+    }
+
+    clearComplete = () => {
+        const incompleteTodos = this.todos.filter(todo => !todo.completed);
+        this.todos.replace(incompleteTodos);
+    }
 }
 
 var store = window.store = new TodoStore;
@@ -10,6 +36,5 @@ var store = window.store = new TodoStore;
 export default store;
 
 autorun(() => {
-    console.log(store.filter);
-    console.log(store.todos[0]);
+    console.log(store.todos);
 });
